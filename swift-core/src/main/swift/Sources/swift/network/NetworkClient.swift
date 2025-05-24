e395ae5d576b2acb03dbe81b0b11ee809f108019
Roman Chumachenko <rchumachenko@readdle.com>
2025-05-24 12:14:14 +0200

    Refactored network client.

--- swift-core/src/main/swift/Sources/swift/network/NetworkClient.swift
+++ swift-core/src/main/swift/Sources/swift/network/NetworkClient.swift
@@ -1,5 +1,9 @@
-import FoundationNetworking
 import Foundation
+
+// Added to silence editor errors about unknown module. This module is separate only for non-Apple platforms.
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
 
 public protocol ApiKeyProvider {
 
@@ -7,14 +11,24 @@ public protocol ApiKeyProvider {
 }
 
 public enum HttpMethod: String {
-        case get = "GET"
-        case post = "POST"
-        case put = "PUT"
-        case delete = "DELETE"
+    case GET
+    case POST
+    case PUT
+    case DELETE
+}
+
+/// Simple empty type for API calls that don't return a response body
+public struct Empty: Codable {}
+
+public enum RequestError: Error {
+    case invalidUrl
+    case requestFailed(code: Int, message: String)
+    case responseParsingFailed(reason: String)
+    case unknownError(reason: String)
 }
 
 public class NetworkClient {
-    
+
     static let TAG = "NetworkClient"
 
     private let baseUrl: String
@@ -36,24 +50,38 @@ public class NetworkClient {
         return URLSession(configuration: configuration)
     }()
 
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+
     public var logger: Logger?
 
-    public init(_ baseUrl: String,_ apiKeyProvider: ApiKeyProvider) {
+    public init(_ baseUrl: String, _ apiKeyProvider: ApiKeyProvider) {
         self.apiKeyProvider = apiKeyProvider
         self.baseUrl = baseUrl
     }
 
     public func testRequest() {
-        guard let request = getRequest(fromEndpoint: "/3/discover/movie", method: .get) else {
-            logger?.log("Failed to create request", withTag: NetworkClient.TAG)
+        guard let request = getRequest(fromEndpoint: "/3/discover/movie", method: .GET) else {
+            logMessage("Failed to create request")
             return
         }
-        execute(request: request)
+        // Explicitly use Empty for the response type when you don't expect a specific response
+        execute(request: request, responseType: Empty.self)
     }
 
-    private func getRequest(fromEndpoint endpoint: String, method: HttpMethod, body: Data? = nil) -> URLRequest? {
+    // Helper method to make it easier to call execute with a specific response type
+    private func execute<Response: Codable>(
+        request: URLRequest, responseType: Response.Type,
+        completion: ((Response?, RequestError?) -> Void)? = nil
+    ) {
+        execute(request: request, completion)
+    }
+
+    private func getRequest(fromEndpoint endpoint: String, method: HttpMethod, body: Data? = nil)
+        -> URLRequest?
+    {
         guard let url = URL(string: "\(baseUrl)\(endpoint)") else {
-            logger?.log("Invalid URL", withTag: NetworkClient.TAG)
+            logMessage("Invalid URL")
             return nil
         }
         return getRequest(url: url, method: method, body: body)
@@ -62,7 +90,8 @@ public class NetworkClient {
     private func getRequest(url: URL, method: HttpMethod, body: Data? = nil) -> URLRequest {
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
-        request.addValue("Bearer \(apiKeyProvider.getApiKey())", forHTTPHeaderField: "Authorization")
+        request.addValue(
+            "Bearer \(apiKeyProvider.getApiKey())", forHTTPHeaderField: "Authorization")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         if let body = body {
             request.httpBody = body
@@ -73,45 +102,84 @@ public class NetworkClient {
         return request
     }
 
-    private func execute(request: URLRequest) {
-        logger?.log("About to send request: \(request.httpMethod ?? "unknown") to URL: \(request.url?.absoluteString ?? "nil URL") with headers \(request.allHTTPHeaderFields ?? [:])", withTag: NetworkClient.TAG)
+    private func execute<Response: Decodable>(
+        request: URLRequest, _ completion: ((Response?, RequestError?) -> Void)? = nil
+    ) {
+        logMessage(
+            "About to send request: \(request.httpMethod ?? "unknown") to URL: \(request.url?.absoluteString ?? "nil URL") with headers \(request.allHTTPHeaderFields ?? [:])"
+        )
 
         let task = urlSession.dataTask(with: request) { data, urlResponse, error in
             // Check error
             if let error = error {
-                self.logger?.log("Error: \(error.localizedDescription) (code: \(error._code), domain: \(error._domain))", withTag: NetworkClient.TAG)
+                let error = RequestError.unknownError(reason: String(describing: error))
+                self.logError(error)
+                completion?(nil, error)
                 return
             }
 
-            // Type-cast the URLResponse to HTTPURLResponse to get more details
-            if let httpResponse = urlResponse as? HTTPURLResponse {
-                self.logger?.log("Got HTTP response: status code \(httpResponse.statusCode), headers: \(httpResponse.allHeaderFields)", withTag: NetworkClient.TAG)
-            } else {
-                self.logger?.log("Got non-HTTP response or nil response: \(String(describing: urlResponse))", withTag: NetworkClient.TAG)
-            }
-            
-            // Check data specifically
-            if let data = data {
-                self.logger?.log("Received data: \(data.count) bytes", withTag: NetworkClient.TAG)
-            } else {
-                self.logger?.log("No data received (data is nil)", withTag: NetworkClient.TAG)
-            }
-            
-            // Process data if we have it
-            guard let data = data, data.count > 0 else {
-                self.logger?.log("No data to process", withTag: NetworkClient.TAG)
+            if let error = self.checkResponse(urlResponse) {
+                self.logError(error)
+                completion?(nil, error)
                 return
             }
-            
-            // Try to parse the data as string
-            if let stringResponse = String(data: data, encoding: .utf8) {
-                self.logger?.log("Success! Response: \(stringResponse)", withTag: NetworkClient.TAG)
-            } else {
-                self.logger?.log("Success, but could not parse response as UTF-8 string. Raw data: \(data as NSData)", withTag: NetworkClient.TAG)
+
+            let response = self.parseResponse(data, responseType: Response.self)
+            switch response {
+            case .success(let parsedResponse):
+                self.logMessage("Request succeeded with response: \(parsedResponse)")
+                completion?(parsedResponse, nil)
+            case .failure(let error):
+                self.logError(error)
+                completion?(nil, error)
             }
         }
-    
-        task.resume()
 
+        task.resume()
+    }
+
+    private func checkResponse(_ response: URLResponse?) -> RequestError? {
+        guard let httpResponse = response as? HTTPURLResponse else {
+            return .unknownError(
+                reason:
+                    "Response is not of type HTTPURLResponse or nil: \(String(describing: response))"
+            )
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            return .requestFailed(
+                code: httpResponse.statusCode,
+                message: "Request failed with status code \(httpResponse.statusCode)")
+        }
+
+        return nil
+    }
+
+    private func parseResponse<Response: Decodable>(_ data: Data?, responseType: Response.Type)
+        -> Result<Response, RequestError>
+    {
+        do {
+            let response = try decoder.decode(Response.self, from: data!)
+            return .success(response)
+        } catch {
+            let plaintextDataContent =
+                if let data = data {
+                    String(data: data, encoding: .utf8) ?? "nil"
+                } else {
+                    "nil"
+                }
+
+            let reason =
+                "Failed to parse response: \(error.localizedDescription). Data: \(String(describing: data)), \(plaintextDataContent)"
+            return .failure(.responseParsingFailed(reason: reason))
+        }
+    }
+
+    private func logMessage(_ message: String) {
+        logger?.log(message, withTag: NetworkClient.TAG)
+    }
+
+    private func logError(_ error: RequestError) {
+        logMessage(String(describing: error))
     }
 }
